5110ea43590d5dbbbb4baaf2ab1d82e30f844c3e
AdelLarbi <larbi.adel@yahoo.fr>
2017-10-26 20:23:08 +0200

    started working on sprint-1.1 branch

--- app/src/main/java/fr/upmc/boteam/obo_app/MainActivity.java
+++ app/src/main/java/fr/upmc/boteam/obo_app/MainActivity.java
@@ -2,12 +2,42 @@ package fr.upmc.boteam.obo_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    Client socket = new Client("192.168.42.91", 3000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        socket.setClientCallback(new Client.ClientCallback () {
+            @Override
+            public void onMessage(String message) {
+                Log.i("MainActivity", message);
+                socket.disconnect();
+            }
+
+            @Override
+            public void onConnect(Socket s) {
+                socket.send("ggg");
+            }
+
+            @Override
+            public void onDisconnect(Socket socket, String message) {
+
+            }
+
+            @Override
+            public void onConnectError(Socket socket, String message) {
+
+            }
+        });
+
+        socket.connect();
     }
 }
